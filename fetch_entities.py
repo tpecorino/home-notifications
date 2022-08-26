a33ff54c0d835afab2769a2bbc0a1030a0f1ac2e
tpecorino <webcodedevelop@protonmail.com>
2022-08-25 21:34:35 -0400

    add gui and database

---
+++ fetch_entities.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def map_entities(state):
+    return state["entity_id"]
+
+
+def fetch_entities(url, access_token):
+    headers = {"Content-Type": "application/json", "Authorization": "Bearer {}".format(access_token)}
+    r = requests.get(url, headers=headers)
+    states = r.json()
+    entities = map(map_entities, states)
+    return list(entities)
